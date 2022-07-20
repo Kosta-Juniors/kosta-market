c213fdcb24840c38283ad3ca9bf1eab196ab5e5c
Wankyu Jung <skyship36@gmail.com>
2022-07-20 20:20:18 +0900

    fix(DB): DB Table 생성 SQL 에 Unique, Not Null 조건 추가 - #18
    TBL_USER.user_id 에 Unique 속성 추가
    TBL_ORDER.payment_date 를 제외한 모든 컬럼에 Not Null 속성 추가
    
    Closes #18

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,81 +1,81 @@
-CREATE DATABASE IF NOT EXISTS kosta_market;
+DROP DATABASE IF EXISTS kosta_market;
+CREATE DATABASE kosta_market;
 ALTER DATABASE kosta_market DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 USE kosta_market;
 
-DROP TABLE IF EXISTS TBL_ORDER CASCADE;
-DROP TABLE IF EXISTS TBL_USER_ORDER CASCADE;
-DROP TABLE IF EXISTS TBL_CATEGORY_PRODUCT CASCADE;
-DROP TABLE IF EXISTS TBL_CATEGORY CASCADE;
-DROP TABLE IF EXISTS TBL_SELLER_PRODUCT CASCADE;
-DROP TABLE IF EXISTS TBL_PRODUCT CASCADE;
-DROP TABLE IF EXISTS TBL_SELLER CASCADE;
-DROP TABLE IF EXISTS TBL_DELIVERY CASCADE;
-DROP TABLE IF EXISTS TBL_USER CASCADE;
-
-CREATE TABLE IF NOT EXISTS TBL_USER (
-  user_no int primary key AUTO_INCREMENT,
-  username varchar(255),
-  password varchar(255),
-  name varchar(255),
-  contact varchar(255)
+CREATE TABLE TBL_USER (
+  user_id int PRIMARY KEY AUTO_INCREMENT,
+  username varchar(255) UNIQUE,
+  password varchar(255) NOT NULL,
+  name varchar(255) NOT NULL,
+  contact varchar(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS TBL_DELIVERY (
-  delivery_no int primary key AUTO_INCREMENT,
-  user_no int,
-  delivery_place varchar(255),
-  FOREIGN KEY (user_no) references TBL_USER(user_no)
+CREATE TABLE TBL_ADDRESS (
+  address_id int PRIMARY KEY AUTO_INCREMENT,
+  user_id int,
+  delivery_place varchar(255) NOT NULL,
+  is_default_address char(1) NOT NULL,
+  FOREIGN KEY (user_id) references TBL_USER(user_id)
 );
 
 CREATE TABLE TBL_SELLER (
-  seller_no int primary key AUTO_INCREMENT,
-  user_no int,
-  business_no varchar(255),
-  FOREIGN KEY (user_no) references TBL_USER(user_no)
+  seller_id int PRIMARY KEY AUTO_INCREMENT,
+  user_id int,
+  business_id varchar(255) NOT NULL,
+  FOREIGN KEY (user_id) references TBL_USER(user_id)
 );
 
 CREATE TABLE TBL_PRODUCT (
-  product_no int primary key AUTO_INCREMENT,
-  product_name varchar(255),
-  product_price int,
-  product_img_file_name varchar(255),
-  product_img_path varchar(255),
-  product_description varchar(255),
-  product_quantity int
+  product_id int PRIMARY KEY AUTO_INCREMENT,
+  product_name varchar(255) NOT NULL,
+  product_price int NOT NULL,
+  product_img_file_name varchar(255) NOT NULL,
+  product_img_path varchar(255) NOT NULL,
+  product_description varchar(255) NOT NULL,
+  product_quantity int NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS TBL_SELLER_PRODUCT (
-  seller_no int,
-  product_no int,
-  FOREIGN KEY (seller_no) references TBL_SELLER(seller_no),
-  FOREIGN KEY (product_no) references TBL_PRODUCT(product_no)
+CREATE TABLE TBL_SELLER_PRODUCT (
+  seller_product_id int PRIMARY KEY AUTO_INCREMENT,
+  seller_id int,
+  product_id int,
+  FOREIGN KEY (seller_id) references TBL_SELLER(seller_id),
+  FOREIGN KEY (product_id) references TBL_PRODUCT(product_id)
 );
 
-CREATE TABLE IF NOT EXISTS TBL_CATEGORY (
-  category_no int primary key AUTO_INCREMENT,
-  category_name varchar(255)
+CREATE TABLE TBL_CATEGORY (
+  category_id int PRIMARY KEY AUTO_INCREMENT,
+  category_name varchar(255) UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS TBL_CATEGORY_PRODUCT (
-  category_no int,
-  product_no int,
-  FOREIGN KEY (category_no) references TBL_CATEGORY(category_no),
-  FOREIGN KEY (product_no) references TBL_PRODUCT(product_no)
+CREATE TABLE TBL_PRODUCT_CATEGORY (
+  product_category_id int PRIMARY KEY AUTO_INCREMENT,
+  product_id int,
+  category_id int,
+  FOREIGN KEY (product_id) references TBL_PRODUCT(product_id),
+  FOREIGN KEY (category_id) references TBL_CATEGORY(category_id)
 );
 
 CREATE TABLE TBL_ORDER (
-  order_no int primary key AUTO_INCREMENT,
-  order_quantity int,
-  order_price int,
-  order_date date,
-  payment_method int,
-  order_state int
+  order_id int PRIMARY KEY AUTO_INCREMENT,
+  product_id int,
+  order_quantity int NOT NULL,
+  order_price int NOT NULL,
+  order_date date NOT NULL,
+  payment_date date,
+  payment_method int NOT NULL,
+  order_state int NOT NULL,
+  FOREIGN KEY (product_id) references TBL_PRODUCT(product_id)
 );
 
 CREATE TABLE TBL_USER_ORDER (
-  user_no int,
-  product_no int,
-  FOREIGN KEY (user_no) references TBL_USER(user_no),
-  FOREIGN KEY (product_no) references TBL_PRODUCT(product_no)
+  user_order_id int PRIMARY KEY AUTO_INCREMENT,
+  user_id int,
+  product_id int,
+  address_id int,
+  FOREIGN KEY (user_id) references TBL_USER(user_id),
+  FOREIGN KEY (product_id) references TBL_PRODUCT(product_id),
+  FOREIGN KEY (address_id) references TBL_ADDRESS(address_id)
 );
