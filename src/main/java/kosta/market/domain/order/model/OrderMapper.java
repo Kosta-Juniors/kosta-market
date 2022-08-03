7dca287ac48f82fd73ffb0d33716ae49ec1d8a6c
Hoony <xognsdl710@naver.com>
2022-08-03 20:23:09 +0900

    feature(주문-모델): 주문 도메인 SQL Mapper 기능 추가 - #64
    Closes #64

--- src/main/java/kosta/market/domain/order/model/OrderMapper.java
+++ src/main/java/kosta/market/domain/order/model/OrderMapper.java
@@ -1,6 +1,9 @@
 package kosta.market.domain.order.model;
 
 import java.util.List;
+
+import kosta.market.domain.product.model.Product;
+import kosta.market.domain.user.model.Address;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -10,14 +13,74 @@ import org.apache.ibatis.annotations.Update;
 @Mapper
 public interface OrderMapper {
 
-	@Insert("INSERT INTO TBL_ORDER VALUES (null, #{productId}, #{orderQuantity}, now(), #{orderState}")
-	int insertOrder(int productId, int orderQuantity, int orderState);
+	//	orderproduct.jsp 페이지에서 주소정보 출력
+	@Select("SELECT * FROM TBL_ADDRESS WHERE user_id = #{userId}")
+	List<Address> selectAddressByUserId(@Param("userId") int userId);
+
+	@Update("UPDATE TBL_PRODUCT SET product_quantity=product_quantity-#{orderQuantity} WHERE product_id=#{productId}")
+	int updateProduct(@Param("productId") int productId,
+					  @Param("orderQuantity") int orderQuantity);
+
+
+	@Insert("INSERT INTO TBL_ORDER VALUES (null, #{productId}, #{orderQuantity}, now(), 0)")
+	int insertOrder(@Param("productId") int productId,
+					@Param("orderQuantity") int orderQuantity);
+	// int orderState 를 0(주문완료)로 입력
 
 	@Insert("INSERT INTO TBL_PAYMENT VALUES (null, #{paymentPrice}, #{paymentMethod}, now())")
-	int insertPayment(@Param("paymentPrice") int paymentPrice, @Param("paymentMethod") String paymentMethod);
+	int insertPayment(@Param("paymentPrice") int paymentPrice,
+					  @Param("paymentMethod") String paymentMethod);
+
+	// 가장 최근에 생성된 PK를  foreign 키로 받아오기
+	@Select("SELECT LAST_INSERT_ID()")
+	int lastInsertId();
 
 	@Insert("INSERT INTO TBL_ORDER_PAYMENT VALUES (null, #{orderId}, #{paymentId})")
-	int insertOrderPayment(@Param("orderId") int orderId, @Param("paymentId") int paymentId);
+	int insertOrderPayment(@Param("orderId") int orderId,
+						   @Param("paymentId") int paymentId);
+
+
+	@Insert("INSERT INTO TBL_USER_ORDER VALUES (null, #{user_id}, #{order_id}, #{address_id})")
+	int insertUserOrder(@Param("user_id") int user_id,
+						@Param("order_id") int order_id,
+						@Param("address_id") int address_id);
+
+	// 여기까지가 Service에서 addOrder()에 사용하는 mapper
+
+	//(Product table)상품상세정보 - Product단의 mapper
+
+	//(Address table)주소지선택 - User단의 mapper
+
+
+	@Select("select * from TBL_ORDER order by order_id desc  limit 1")
+	int selectOrderId();
+
+
+
+//	@Select("select A.*, B.product_name, D.payment_price, D.payment_method, F.delivery_place, G.name, G.contact" +
+//			" from TBL_ORDER as A" +
+//			" left join TBL_PRODUCT as B on A.product_id=B.product_id" +
+//			" left join TBL_ORDER_PAYMENT as C on A.order_id=C.order_id" +
+//			" left join TBL_PAYMENT as D on C.payment_id=D.payment_id" +
+//			" left join TBL_USER_ORDER as E on A.order_id=E.order_id" +
+//			" left join TBL_ADDRESS as F on E.address_id=F.address_id" +
+//			" left join TBL_USER as G on E.user_id=G.user_id" +
+//			"where A.order_id=#{order_id}")
+//	OrderDetailDto selectOrder(@Param("order_id") int order_id);
+
+
+	@Select("SELECT A.*, B.product_name, D.payment_price, D.payment_method, F.delivery_place, G.name, G.contact" +
+			" FROM TBL_ORDER as A" +
+			" LEFT JOIN TBL_PRODUCT AS B ON A.product_id=B.product_id" +
+			" LEFT JOIN TBL_ORDER_PAYMENT AS C ON A.order_id=C.order_id" +
+			" LEFT JOIN TBL_PAYMENT AS D ON C.payment_id=D.payment_id" +
+			" LEFT JOIN TBL_USER_ORDER AS E ON A.order_id=E.order_id" +
+			" LEFT JOIN TBL_ADDRESS AS F ON E.address_id=F.address_id" +
+			" LEFT JOIN TBL_USER AS G ON E.user_id=G.user_id" +
+			" WHERE A.order_id=#{order_id}")
+	OrderDetailDto selectOrder(@Param("order_id") int order_id);
+
+
 
 	@Select("SELECT * FROM TBL_ORDER AS A JOIN"
 		+ " TBL_USER_ORDER AS B ON A.order_id = B.order_id"
@@ -29,14 +92,17 @@ public interface OrderMapper {
 		+ " WHERE B.seller_id = #{sellerId}")
 	List<Order> selectOrderListBySellerId(@Param("sellerId") int sellerId);
 
-	@Select("SELECT * FROM TBL_ORDER WHERE order_id = #{orderId}")
-	Order selectOrder(@Param("orderId") int orderId);
+
 
 	@Select("SELECT * FROM TBL_PAYMENT WHERE payment_id = #{paymentId}")
 	Payment selectPayment(@Param("paymentId") int paymentId);
 
 	@Update("UPDATE TBL_ORDER SET order_state = #{orderState}")
 	int updateOrder(@Param("orderState") int orderState);
+
+	@Select("SELECT * FROM TBL_PRODUCT WHERE product_id=#{productId}")
+	Product selectProduct(@Param("productId")int productId);
+
 }
 
 
