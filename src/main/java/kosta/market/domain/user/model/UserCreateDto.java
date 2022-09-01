e17f348f9901e21440f947434832d49846cad69a
Wankyu Jung <skyship36@gmail.com>
2022-09-01 10:29:15 +0900

    feature(회원-모델): 유저 도메인에 누락된 클래스 추가 - #97
    문제 : User, UserCreateDto, UserProcessController 파일이 누락되어 import 오류가 발생함
    해결 : 위 3개 클래스를 새롭게 추가함
    
    Closes #97

---
+++ src/main/java/kosta/market/domain/user/model/UserCreateDto.java
@@ -0,0 +1,17 @@
+package kosta.market.domain.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCreateDto {
+	private String username;
+	private String password;
+	private String name;
+	private String contact;
+}
